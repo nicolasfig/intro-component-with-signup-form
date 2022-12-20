5421276d688f6d1af0fd1b560c3f5f12a83ab817
Nicolas Figueroa <nicolasfigb@gmail.com>
2022-12-19 23:35:19 -0500

    Added base script

---
+++ app.js
@@ -0,0 +1,10 @@
+/* Getting input fields*/
+
+const name = document.querySelector(".name");
+const lastname = document.querySelector(".lastname");
+const email = document.querySelector(".email");
+const password = document.querySelector(".password");
+
+/* Getting buttons */
+const trailBtn = document.querySelector(".trial-btn");
+const submitBtn = document.querySelector(".submit-btn");
