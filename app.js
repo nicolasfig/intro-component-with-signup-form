8f4b4158169970d564db4eb0761864f8709d4da7
Nicolas Figueroa <nicolasfigb@gmail.com>
2022-12-20 22:14:00 -0500

    Basic input validation

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 /* Getting input fields*/
 
-const name = document.querySelector(".name");
+const nameInput = document.querySelector(".name");
+const msgName = document.querySelector(".msg-name");
 const lastname = document.querySelector(".lastname");
+const msgLastName = document.querySelector(".msg-lastname");
 const email = document.querySelector(".email");
+const msgEmail = document.querySelector("msg-email");
 const password = document.querySelector(".password");
-
+const msgPass = document.querySelector(".msg-password");
 /* Getting buttons */
 const trailBtn = document.querySelector(".trial-btn");
 const submitBtn = document.querySelector(".submit-btn");
 
 const validation = () => {
-  console.log("Hello");
+  if (nameInput.value === "") {
+    msgName.innerHTML = "Name cannot be empty";
+  }
+  if (lastname.value === "") {
+    msgLastName.innerHTML = "Last Name cannot be empty";
+  }
+  if (email.value === "") {
+    msgEmail.innerHTML = "Email cannot be empty";
+  }
+  if (password.value === "") {
+    msgPass.innerHTML = "Password cannot be empty";
+  }
 };
 
 submitBtn.addEventListener("click", () => {
