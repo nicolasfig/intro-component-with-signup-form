c138134633dd89ead7b0109136cbb34c01efa23e
Nicolas Figueroa <nicolasfigb@gmail.com>
2022-12-20 22:24:53 -0500

    Basic input validation and styles

--- app.js
+++ app.js
@@ -2,12 +2,16 @@
 
 const nameInput = document.querySelector(".name");
 const msgName = document.querySelector(".msg-name");
+
 const lastname = document.querySelector(".lastname");
 const msgLastName = document.querySelector(".msg-lastname");
+
 const email = document.querySelector(".email");
-const msgEmail = document.querySelector("msg-email");
+const msgEmail = document.querySelector(".msg-email");
+
 const password = document.querySelector(".password");
 const msgPass = document.querySelector(".msg-password");
+
 /* Getting buttons */
 const trailBtn = document.querySelector(".trial-btn");
 const submitBtn = document.querySelector(".submit-btn");
